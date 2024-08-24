d605530c4bbf10613756be03d64511ef67c6ced0
ozvann <ozvannabraham@gmail.com>
2024-08-24 16:54:29 +0200

    gravité fixé pour la balle mais pas pour le gauche droite a cause de colision vers un mur (joueur)

--- pong/code/balle.gd
+++ pong/code/balle.gd
@@ -1,28 +1,36 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = 0
+const SPEED = 150
+const GRAV = 190
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var col = 0
+var mur = 0
+
 
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	
+	if is_on_ceiling():
+		col += 1
+	if is_on_floor():
+		col -= 1
+	
+	if col:
+		velocity.y = GRAV
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = -GRAV
+	
+	
+	if is_on_wall():
+		mur += 1
+	if is_on_wall():
+		mur -= 1
+	
+	if mur:
+		velocity.x = SPEED
+	else:
+		velocity.x = -SPEED
 
 	move_and_slide()
