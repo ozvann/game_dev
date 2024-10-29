355ac9b825edfd5cfca7b4d54e4e0e8a8ac6bf11
ozvann <ozvannabraham@gmail.com>
2024-10-29 09:46:50 +0100

    j'ai fais un menu pause

--- laby's_adventure/Code/option.gd
+++ laby's_adventure/Code/option.gd
@@ -6,13 +6,17 @@ func _ready() -> void:
 	visible = false
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if visible == false and Input.is_action_just_pressed("ui_cancel"):
 		visible = true
 	else:
 		if visible == true and Input.is_action_just_pressed("ui_cancel"):
+			get_tree().paused = false
 			visible = false
+	if visible == true:
+		get_tree().paused = true
 
 
 func _on_continuer_pressed() -> void:
+	get_tree().paused = false
 	visible = false
--- laby's_adventure/Code/quitter.gd
+++ laby's_adventure/Code/quitter.gd
@@ -2,11 +2,12 @@ extends Button
 
 
 
-func _ready():
+func _ready() -> void:
 	var button = Button.new()
 	button.text = "QUITTER"
 	button.pressed.connect(self._button_pressed)
 	add_child(button)
 
-func _button_pressed():
+
+func _button_pressed() -> void:
 	get_tree().quit();
