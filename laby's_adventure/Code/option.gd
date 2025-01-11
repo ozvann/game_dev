cd9c7e0d658b71fe375ac14fb5bce21287c1b152
ozvann <ozvannabraham@gmail.com>
2025-01-11 20:22:51 +0100

    Lancement d'un projet platformer 2D après une idée qui m'est passer par la tête

--- laby's_adventure/Code/option.gd
+++ laby's_adventure/Code/option.gd
@@ -9,12 +9,11 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if visible == false and Input.is_action_just_pressed("ui_cancel"):
 		visible = true
+		get_tree().paused = true
 	else:
 		if visible == true and Input.is_action_just_pressed("ui_cancel"):
 			get_tree().paused = false
 			visible = false
-	if visible == true:
-		get_tree().paused = true
 
 
 func _on_continuer_pressed() -> void:
