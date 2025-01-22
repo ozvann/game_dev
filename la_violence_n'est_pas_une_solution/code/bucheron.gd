811a40fbd8a8146284278f0fb6d6c0c2c7b813ae
ozvann <ozvannabraham@gmail.com>
2025-01-22 13:30:05 +0100

    Ajout d'image

--- la_violence_n'est_pas_une_solution/code/bucheron.gd
+++ la_violence_n'est_pas_une_solution/code/bucheron.gd
@@ -18,18 +18,3 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
-
-
-func annimation():
-	if Input.is_action_just_pressed("ui_right"):
-		$AnimationPlayer.play("marche")
-		$Bucheron.flip_h = false
-		$Bras.flip_h = false
-	if Input.is_action_just_pressed("ui_left"):
-		$AnimationPlayer.play("marche")
-		$Bucheron.flip_h = true
-		$Bras.flip_h = true
-	if Input.is_action_just_released("ui_right") or Input.is_action_just_released("ui_left"):
-		$AnimationPlayer.play("idle")
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		$AnimationPlayer.play("saut")
--- pong/code/balle.gd
+++ pong/code/balle.gd
@@ -1,51 +1,10 @@
-extends CharacterBody2D
-
-
-<<<<<<< HEAD
-const SPEED = 250
-const GRAV = 200
-=======
-const SPEED = 300
-const GRAV = 300
->>>>>>> 5a4dec92a6d6393154d4e5cfb73a124c067cca3e
-
-
-var col = 0
-var mur = 0
-var ver = 0
+extends RigidBody2D
 
 
 
-func _physics_process(delta):
-	
-	
-	
-	
-	if is_on_ceiling():
-		col += 1
-		ver += 1
-	if is_on_floor():
-		col -= 1
-		ver += 1
-	
-	if col:
-		velocity.y = GRAV
-	else:
-		velocity.y = -GRAV
-	
-	
-	
-	
-	if is_on_wall() and ver >= 1:
-		if mur == 1:
-			mur = 0
-		else:
-			mur = 1
-		ver = 0
-	
-	if mur:
-		velocity.x = SPEED
-	else:
-		velocity.x = -SPEED
+func _ready() -> void:
+	pass
 
-	move_and_slide()
+
+func _process(delta: float) -> void:
+	pass
